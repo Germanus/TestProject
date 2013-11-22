ecb1d428f21cebdcea6c9cef214431caa1093b5e
Germanus <Germanus@github.com>
2013-11-23 00:58:15 +0400

    вы

---
+++ webapp/src/main/java/Test.java
@@ -0,0 +1,13 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: KAZAKEVICH
+ * Date: 17.11.13
+ * Time: 12:11
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("ffffffff");
+    }
+}
